5e58153d19cd3d8af2e10f5681aa0e8433381f8e
Johan Verrept <johan@verrept.eu>
2021-12-22 16:35:39 +0100

    Fix printer name

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -143,7 +143,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Sovol SV01"
+#define CUSTOM_MACHINE_NAME "Sovol SV03"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
