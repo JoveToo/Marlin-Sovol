15c10adfe0faa538197a43cb7afe87076750068b
Johan Verrept <johan@verrept.eu>
2022-01-29 02:20:48 +0100

    remove baudrate gcode

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -121,7 +121,7 @@
  * :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000]
  */
 #define BAUDRATE 250000
-#define BAUD_RATE_GCODE     // Enable G-code M575 to set the baud rate
+//#define BAUD_RATE_GCODE     // Enable G-code M575 to set the baud rate
 
 /**
  * Select a secondary serial port on the board to use for communication with the host.
